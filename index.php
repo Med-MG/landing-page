535d2d1f81d8d456b6833d4196cbec5115954949
MOUIGUINA MOHAMED <mohamedmouiguina7@gmail.com>
2020-07-12 18:16:23 +0100

    added whatsapp button

--- index.php
+++ index.php
@@ -352,6 +352,7 @@
 	</div>
 	<div class="buy-now-sticky">
 		<a href="#buy-form" class="button button-primary button-sticky">شراء الآن</a>
+		<a href="https://wa.me/212620226054?text=%D8%A3%D8%B1%D9%8A%D8%AF%20%D8%B4%D8%B1%D8%A7%D8%A1%20%D9%85%D8%A8%D8%B1%D8%AF%20%D8%A7%D9%84%D8%AC%D9%88%20%D8%A7%D9%84%D9%85%D8%AD%D9%85%D9%88%D9%84" class="button button-whatsapp button-sticky"><img src="dist/images/icons8-whatsapp-64.png" class="button-whatsapp__img" /> الواتساب</a>
 	</div>
 
 
