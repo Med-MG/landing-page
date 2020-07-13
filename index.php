ee89b19a88ebb668d7a3801d8a3d254f0fae6238
Mohamed Mouiguina <46869106+Med-MG@users.noreply.github.com>
2020-07-13 13:44:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -454,11 +454,7 @@ if (isset($_GET['order'])) {
 		});
 
 	</script>
-	<script type="text/javascript">
-  		$('#$addToCartButton').click(function() {
-    		fbq('track', 'Purchase', {currency: "USD", value: 40.00});
-  		});
-	</script>
+
 
 </body>
 
